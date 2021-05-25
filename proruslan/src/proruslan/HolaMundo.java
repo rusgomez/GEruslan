35c77946a57b1e05447d7c86b843b4215f7ee34d
Propi <Propi@DESKTOP-5E92744>
2021-05-25 16:50:48 +0200

    Tercer Cambio

--- proruslan/src/proruslan/HolaMundo.java
+++ proruslan/src/proruslan/HolaMundo.java
@@ -5,7 +5,12 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HolaMundo");
+		
 		System.out.println("HolaDeNuevo");
+		
+		System.out.println("3 cambio");
+		
+		
 	}
 
 }
