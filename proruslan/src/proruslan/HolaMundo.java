66fb91627b32ea1435481cf54bebc9659f30b705
Propi <Propi@DESKTOP-5E92744>
2021-05-25 17:21:24 +0200

    metodo segundo usuario

--- proruslan/src/proruslan/HolaMundo.java
+++ proruslan/src/proruslan/HolaMundo.java
@@ -2,6 +2,11 @@ package proruslan;
 
 public class HolaMundo {
 
+	int sumar (int a ,int b) {
+		
+		return a + b;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HolaMundo");
