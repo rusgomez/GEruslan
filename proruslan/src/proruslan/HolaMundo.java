ec26b3c51ecf65ab42a5791fa9a5d737bc185dc1
Propi <Propi@DESKTOP-5E92744>
2021-05-25 17:19:11 +0200

    Metodo primer usuario

--- proruslan/src/proruslan/HolaMundo.java
+++ proruslan/src/proruslan/HolaMundo.java
@@ -1,6 +1,12 @@
 package proruslan;
 
 public class HolaMundo {
+	
+	int restar (int a, int b) {
+		
+		return a - b;
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
